f6f51c6478a187822264ccc0fc11b940b0fe332d
DanielHabenicht <daniel-habenicht@outlook.de>
2023-03-07 21:03:25 +0000

    update

--- bug.py
+++ bug.py
@@ -11,19 +11,19 @@ print(df.index[0])
 print(end)
 
 fig, ax = plt.subplots()
-ax.axvspan(
-    start,  # or df.index[0]
-    end,  # or df.index[-1]
-    color="red",
-    alpha=0.1,
-    lw=0,
-    zorder=1,
-)
+# ax.axvspan(
+#     start,  # or df.index[0]
+#     end,  # or df.index[-1]
+#     color="red",
+#     alpha=0.1,
+#     lw=0,
+#     zorder=1,
+# )
 
 # Using the `ax.plot` method works
-# ax.plot(df.index, df["n1"], color="black", alpha=0.2, linestyle="solid", zorder=3)
+# ax.plot(df.index, df["n1"],alpha=0.2, zorder=3)
 
-df.plot(ax=ax, alpha=0.2, linestyle="solid", zorder=3)
+df.plot(ax=ax, alpha=0.2, zorder=3)
 
 # Using `axvspan` before and after the plot actually works too
 # Only using this axvspan method results in a blank graph
